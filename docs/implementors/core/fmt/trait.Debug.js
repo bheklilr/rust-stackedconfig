b8b8d19c7e7fd181e93aef652ac4c8873b28a2ca
Aaron Stevens <bheklilr2@gmail.com>
2018-01-18 22:24:30 -0600

    Trying it this way

---
+++ docs/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["stackedconfig"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"stackedconfig/struct.ConfigStack.html\" title=\"struct stackedconfig::ConfigStack\">ConfigStack</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"stackedconfig/enum.Lookup.html\" title=\"enum stackedconfig::Lookup\">Lookup</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
